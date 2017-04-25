dbd75816db0ae50efc5f368451e87fe134fd4783
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-25 14:09:40 -0700

    Updating this branch with master. Will merge later

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Initialize the directory to be a python module
+"""
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix="/app/v1")
+from api.v1.views.index import *
+#import api.v1.views.states
