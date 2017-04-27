be2b31780b2e7e8d4f38b349d675bbca7845e28c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 00:03:52 -0700

    Updating __init__

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,7 +69,6 @@ class DBStorage:
         gets an object of a certain kind of class
         """
         if cls not in self.__models_available.keys():
-            print("ERROR: {:s} is not an available class".format(type(cls).__name__))
             return (None)
         for class_instance in self.__session.query(
                 self.__models_available[cls]):
